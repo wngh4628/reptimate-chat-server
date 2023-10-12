f72998dc30a04d263c563b45a368bb2d4d2f0ce9
이지훈 <wgd4308@gmail.com>
2023-10-12 15:08:04 +0900

    Fix: 옥션채팅에서 auction_participate이벤트 수신시 오류 수정2

--- dist/domains/auction_chat/repositories/board-auction.repository copy.d.ts
+++
@@ -1,4 +0,0 @@
-import { Repository } from 'typeorm';
-import { ChatConversation } from '../entities/chat-conversation.entity';
-export declare class ChatConversationRepository extends Repository<ChatConversation> {
-}
