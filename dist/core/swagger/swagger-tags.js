ed0929941b85707a960e4e46b91ca9557d252558
이지훈 <wgd4308@gmail.com>
2023-12-18 18:05:03 +0900

    Test: 배포 테스트2

--- dist/core/swagger/swagger-tags.js
+++ dist/core/swagger/swagger-tags.js
@@ -5,7 +5,7 @@ var SwaggerTag;
 (function (SwaggerTag) {
     SwaggerTag["AUTH"] = "Auth: \uC778\uC99D";
     SwaggerTag["USER"] = "User: \uC720\uC800";
-    SwaggerTag["PERSONALCHAT"] = "1:1 \uCC44\uD305!";
+    SwaggerTag["PERSONALCHAT"] = "1:1 \uCC44\uD305";
     SwaggerTag["AUCTIONCHAT"] = "Auction \uCC44\uD305";
     SwaggerTag["LIVE"] = " Live Stream \uCC44\uD305";
     SwaggerTag["APP"] = "App: \uAE30\uD0C0 \uC11C\uBC84\uAD00\uB828";
--- src/core/swagger/swagger-tags.ts
+++ src/core/swagger/swagger-tags.ts
@@ -2,7 +2,7 @@ export enum SwaggerTag {
   // 유저 관련
   AUTH = 'Auth: 인증',
   USER = 'User: 유저',
-  PERSONALCHAT = '1:1 채팅!',
+  PERSONALCHAT = '1:1 채팅',
   AUCTIONCHAT = 'Auction 채팅',
   LIVE = ' Live Stream 채팅',
   // 시스템 관련 / 기타
